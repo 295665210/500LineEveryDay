31e8e3e16bfe07d740a35d32d9c989ffeab6f4d0
295665210 <295665210@qq.com>
2020-04-27 21:48:57 +0800

    2020年4月27日

---
+++ WPF/LayoutPanels/LocalizableText.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LayoutPanels
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class LocalizableText : Window
+    {
+        public LocalizableText()
+        {
+            InitializeComponent();
+        }
+
+
+        private void ChkLongText_OnUnchecked(object sender, RoutedEventArgs e)
+        {
+            cmdPrev.Content = "<- Go to the Previous Window";
+            cmdNext.Content = "Go to the Next Window ->";
+        }
+
+        private void ChkLongText_OnChecked(object sender, RoutedEventArgs e)
+        {
+            cmdPrev.Content = "Prev";
+            cmdNext.Content = "Next";
+        }
+    }
+}
