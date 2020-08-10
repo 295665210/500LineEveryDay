0c3c7ea5a7517ae3026575cc2b395a63a65a6464
295665210 <295665210@qq.com>
2020-08-10 22:59:42 +0800

    2020年8月10日22:59:37 打卡。

---
+++ CodeInSDK/CodeInSDK/ExternalResourceDBServer/Application.cs
@@ -0,0 +1,39 @@
+﻿// using System;
+// using System.Collections.Generic;
+// using System.Linq;
+// using System.Text;
+// using System.Threading.Tasks;
+// using Autodesk.Revit.ApplicationServices;
+// using Autodesk.Revit.DB;
+// using Autodesk.Revit.DB.ExternalService;
+// using Autodesk.Revit.UI;
+//
+// namespace CodeInSDK.ExternalResourceDBServer
+// {
+//     public class DBApplication : IExternalDBApplication
+//     {
+// #region IExternaDBApplication Members
+//         public ExternalDBApplicationResult OnStartup(ControlledApplication application)
+//         {
+//             //get revit's ExternalResourceService
+//             ExternalService externalResourceService =
+//                 ExternalServiceRegistry.GetService(ExternalServices.BuiltInExternalServices.ExternalResourceService);
+//
+//             if (externalResourceService == null)
+//             {
+//                 return ExternalDBApplicationResult.Failed;
+//             }
+//
+//             //create an instance of your IExternalResourceServer and register it with the ExternalResourceService.
+//             IExternalResourceServer sampleServer = new SampleExternalResourceDBServer();
+//             externalResourceService.AddServer(sampleServer);
+//             return ExternalDBApplicationResult.Succeeded;
+//         }
+//
+//         public ExternalDBApplicationResult OnShutdown(ControlledApplication application)
+//         {
+//             return ExternalDBApplicationResult.Succeeded;
+//         }
+// #endregion
+//     }
+// }
---
+++ CodeInSDK/CodeInSDK/ExternalResourceDBServer/KeynotesDatabase.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit;
+using Autodesk.Revit.DB;
+
+namespace CodeInSDK.ExternalResourceDBServer
+{
+    static class KeynotesDatabase
+    {
+        /// <summary>
+        /// Indicates what version of keynote data is currently available from the database.
+        /// </summary>
+        public static string CurrentVersion
+        {
+            //Assume that the server's keynote data is updated at the beginning of every month.
+            get { return System.DateTime.Now.ToString("MM-yyyy"); }
+        }
+
+        /// <summary>
+        /// Validates the specified database 'key.'
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static bool IsValidDBKey(String key)
+        {
+            return key == "1" || key == "2" || key == "3" || key == "4";
+        }
+
+        public static void LoadKeynoteEntries(string key, ref KeyBasedTreeEntriesLoadContent kdrlc)
+        {
+
+        }
+    }
+}
