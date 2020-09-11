cce01f2793d6864936f9d24df1a479f85d1ecb72
295665210 <295665210@qq.com>
2020-09-11 14:57:53 +0800

    2020年9月11日14:57:50

--- codeInTangsengjiewa/CodeInTangsengjiewa3/机电/Cmd_CutMepsInPieces.cs
+++ codeInTangsengjiewa/CodeInTangsengjiewa3/机电/Cmd_CutMepsInPieces.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Windows;
 using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.DB.Electrical;
@@ -70,23 +72,138 @@ namespace CodeInTangsengjiewa3.机电
                             continue;
                         }
                         var points = new List<XYZ>();
-                        for (int i = 0; i < length/perlength; i++)
+                        for (int i = 0; i < length / perlength; i++)
                         {
                             var tempoint = temline.StartPoint() + perlength * i * temline.Direction;
                             points.Add(tempoint);
                         }
-                        var temmeplist =new List<ElementId>();
-                        Breakm
+                        var temmeplist = new List<ElementId>();
+                        BreakMep(mep, points, ref temmeplist);
+                        foreach (ElementId mepid in temmeplist)
+                        {
+                            foreach (ElementId mepid2 in temmeplist)
+                            {
+                                var temmep = mepid.GetElement(doc) as MEPCurve;
+                                var temmep2 = mepid2.GetElement(doc) as MEPCurve;
+                                var line1 = temmep.LocationLine();
+                                var line2 = temmep2.LocationLine();
+                                var cons1 = temmep.ConnectorManager.Connectors;
+                                var cons2 = temmep2.ConnectorManager.Connectors;
+                                var enu1 = cons1.GetEnumerator();
+                                var enu2 = cons2.GetEnumerator();
+                                enu2.Reset();
+                                while (enu1.MoveNext())
+                                {
+                                    enu2.Reset();
+                                    while (enu2.MoveNext())
+                                    {
+                                        var con1 = enu1.Current as Connector;
+                                        var con2 = enu2.Current as Connector;
+                                        var origin1 = con1?.Origin;
+                                        var origin2 = con2?.Origin;
+                                        if (origin1 != null && origin2 != null && origin1.IsAlmostEqualTo(origin2))
+                                        {
+                                            doc.Create.NewUnionFitting(con1, con2);
+                                        }
+                                    }
+                                }
+                            }
+                        }
                     }
-
-
+                    ts.Commit();
                 }
                 catch (Exception e)
                 {
-                    Console.WriteLine(e);
-                    throw;
+                    MessageBox.Show(e.ToString());
                 }
             }
+            return Result.Succeeded;
+        }
+
+        private void BreakMep(MEPCurve mep, List<XYZ> points, ref List<ElementId> meplist)
+        {
+            if (points.Count < 1)
+            {
+                return;
+            }
+            if (!meplist.Contains(mep.Id))
+            {
+                meplist.Add(mep.Id);
+            }
+            var point = points.ElementAt(0);
+            var leftpoints = points.Remove(point);
+            MEPCurve newmep = null;
+            var flag1 = point.IsOnLine(mep.LocationLine());
+            var flag2 = mep.LocationLine().StartPoint().DistanceTo(point) > 1d.MetricToFeet();
+            var flag3 = mep.LocationLine().EndPoint().DistanceTo(point) > 1d.MetricToFeet();
+
+            if (flag1 && flag2 && flag3)
+            {
+                newmep = mep.BreakCurve(point);
+            }
+            if (newmep != null)
+            {
+                meplist.Add(newmep.Id);
+            }
+            var pointsonmep = points.Where(m => m.IsOnLine(mep.LocationLine())).ToList();
+            if (pointsonmep.Count > 0)
+            {
+                BreakMep(mep, pointsonmep, ref meplist);
+            }
+            var pointsonnewmep = new List<XYZ>();
+            if (newmep != null)
+            {
+                pointsonnewmep = points.Where(m => m.IsOnLine(newmep.LocationLine())).ToList();
+                if (pointsonnewmep.Count > 0)
+                {
+                    BreakMep(newmep, pointsonnewmep, ref meplist);
+                }
+            }
+        }
+    }
+
+    public static class TemExtension
+    {
+        public static MEPCurve BreakCurve(this MEPCurve mep, XYZ point)
+        {
+            var locationLine = mep.LocationLine();
+            var start = locationLine.StartPoint();
+            var end = locationLine.EndPoint();
+            var executeflag = point.IsOnLine(locationLine) && point.DistanceTo(start) > 1d.MetricToFeet() &&
+                point.DistanceTo(end) > 1d.MetricToFeet();
+            if (!executeflag)
+            {
+                throw new Exception("点不在线上");
+            }
+            var doc = mep.Document;
+
+#if Revit2016
+                        return null;
+#endif
+#if Revit2019
+            ElementId result = null;
+            if (mep is Duct)
+            {
+                result = MechanicalUtils.BreakCurve(doc, mep.Id, point);
+            }
+            else if (mep is Pipe)
+            {
+                result = PlumbingUtils.BreakCurve(doc, mep.Id, point);
+            }
+            else if (mep is CableTray)
+            {
+                var newline1 = Line.CreateBound(start, point);
+                var newline2 = Line.CreateBound(point, end);
+                (mep.Location as LocationCurve).Curve = newline1;
+                var newcabletray = CableTray.Create(doc, mep.GetTypeId(), point, end, mep.ReferenceLevel.Id);
+                var para_w = newcabletray.get_Parameter(BuiltInParameter.RBS_CABLETRAY_WIDTH_PARAM);
+                var para_h = newcabletray.get_Parameter(BuiltInParameter.RBS_CABLETRAY_HEIGHT_PARAM);
+                para_w.Set(mep.Width);
+                para_h.Set(mep.Height);
+                result = newcabletray.Id;
+            }
+            return result.GetElement(doc) as MEPCurve;
+#endif
         }
     }
 }
---
+++ codeInTangsengjiewa/CodeInTangsengjiewa3/机电/Cmd_ExtendWire.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Electrical;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using CodeInTangsengjiewa3.BinLibrary.Extensions;
+
+namespace CodeInTangsengjiewa3.机电
+{
+    /// <summary>
+    /// 延长导线命令
+    /// </summary>
+    [Transaction(TransactionMode.Manual)]
+    [Journaling(JournalingMode.UsingCommandData)]
+    [Regeneration(RegenerationOption.Manual)]
+    class Cmd_ExtendWire : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            var uiapp = commandData.Application;
+            var uidoc = uiapp.ActiveUIDocument;
+            var doc = uidoc.Document;
+            var sel = uidoc.Selection;
+            var acview = doc.ActiveView;
+
+            while (true)
+            {
+                try
+                {
+                    var wireref = sel.PickObject(ObjectType.Element, doc.GetSelectionFilter(m => m is Wire));
+                    var wire = wireref.GetElement(doc) as Wire;
+                    var locationCurve = wire.Location as LocationCurve;
+
+                    var intersection = default(XYZ);
+                    var tempoint = sel.PickPoint();
+                    var linefirst = Line.CreateBound(wire.GetVertex(0), wire.GetVertex(1));
+                    var vertexcount = wire.NumberOfVertices;
+                    var linelast = Line.CreateBound(wire.GetVertex(vertexcount - 2), wire.GetVertex(vertexcount - 1));
+                    var globalpoint = wireref.GlobalPoint;
+
+                    globalpoint = new XYZ(globalpoint.X, globalpoint.Y, linefirst.StartPoint().Z);
+
+                    if (!globalpoint.IsOnLine(linefirst) && !globalpoint.IsOnLine(linelast))
+                    {
+                        MessageBox.Show("本功能不适应于曲线类型的导线 选择起始端或结束端导线");
+                    }
+
+                    Transaction ts1 = new Transaction(doc, "更改导线");
+                    ts1.Start();
+                    if (vertexcount > 2)
+                    {
+                        if (globalpoint.IsOnLine(linefirst))
+                        {
+                            tempoint = tempoint.ProjectToXLine(linefirst);
+                            wire.SetVertex(0, tempoint);
+                        }
+                        else if (globalpoint.IsOnLine(linelast))
+                        {
+                            tempoint = tempoint.ProjectToXLine(linelast);
+                            wire.SetVertex(vertexcount - 1, tempoint);
+                        }
+                    }
+                    else if (vertexcount == 2)
+                    {
+                        var startpo = wire.GetVertex(0);
+                        var endpo = wire.GetVertex(1);
+                        if (globalpoint.DistanceTo(startpo) < globalpoint.DistanceTo(endpo))
+                        {
+                            tempoint = tempoint.ProjectToXLine(linefirst);
+                            wire.SetVertex(0, tempoint);
+                        }
+                        else
+                        {
+                            tempoint = tempoint.ProjectToXLine(linelast);
+                            wire.SetVertex(vertexcount - 1, tempoint);
+                        }
+                    }
+                    ts1.Commit();
+                }
+                catch (Exception)
+                {
+                    break;
+                }
+            }
+            return Result.Succeeded;
+        }
+    }
+}
