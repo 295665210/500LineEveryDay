2d353ce50a1af88df69a5e19d82fd936a9343b18
295665210 <295665210@qq.com>
2021-03-20 10:02:57 +0800

    2021年3月20日10:02:55

---
+++ CodeInBuildingCoder/CodeInBuildingCoder1/CmdEllipticalArc.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace CodeInBuildingCoder1
+{
+
+    //运行不出来？未将对象引用实例？
+    [Transaction(TransactionMode.Manual)]
+    class CmdEllipticalArc : IExternalCommand
+    {
+        Curve CreateEllipse(Application app) //Ellipse  /ɪˈlɪps/  椭圆
+        {
+            XYZ center = XYZ.Zero;
+            double radX = 30;
+            double radY = 50;
+
+            XYZ xVec = XYZ.BasisX;
+            XYZ yVec = XYZ.BasisY;
+
+            double param0 = 0.0;
+            double param1 = 2 * Math.PI;
+
+            Curve c = Ellipse.CreateCurve(center, radX, radY, xVec, yVec, param0, param1);
+            //Create a line from ellipse center in direction of target angle;
+
+            double targetAngle = Math.PI / 3.0;
+            XYZ direction = new XYZ(Math.Cos(targetAngle), Math.Sign(targetAngle), 0);
+
+            Line line = Line.CreateBound(center, direction);
+
+            //Find intersection between line and ellipse:
+            IntersectionResultArray results;
+            c.Intersect(line, out results);
+            //Find shortest intersection segment:  //segment :段，部分；
+            foreach (IntersectionResult result in results)
+            {
+                double p = result.UVPoint.U;
+                if (p < param1)
+                {
+                    param1 = p;
+                }
+            }
+
+            //Apply parameter to the ellipse:
+            c.MakeBound(param0, param1);
+            return c;
+        }
+
+
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            Application app = commandData.Application.Application;
+            Document doc = commandData.Application.ActiveUIDocument.Document;
+            using (Transaction tx = new Transaction(doc))
+            {
+                tx.Start("arcTest");
+                Curve c = CreateEllipse(app);
+                tx.Commit();
+            }
+
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ CodeInBuildingCoder/CodeInBuildingCoder1/CmdExportImage.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace CodeInBuildingCoder1
+{
+    [Transaction(TransactionMode.Manual)]
+    class CmdExportImage : IExternalCommand
+    {
+        #region SetWhiteRenderBackground
+        void SetWhiteRenderBackground(View3D view)
+        {
+            RenderingSettings rs = view.GetRenderingSettings();
+            rs.BackgroundStyle = BackgroundStyle.Color;
+
+            ColorBackgroundSettings cbs = (ColorBackgroundSettings) rs.GetBackgroundSettings();
+            cbs.Color = new Color(255, 0, 0);
+            rs.SetBackgroundSettings(cbs);
+            view.SetRenderingSettings(rs);
+        }
+
+        static string ExportToImage(Document doc)
+        {
+            var tempFileName = Path.ChangeExtension(Path.GetRandomFileName(), "png");
+            string tempImageFile;
+            try
+            {
+                tempImageFile = Path.Combine(Path.GetTempPath(), tempFileName);
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+            IList<ElementId> views = new List<ElementId>();
+
+            try
+            {
+                var collector = new FilteredElementCollector(doc);
+                var viewFamilyType = collector.OfClass(typeof(ViewFamilyType)).OfType<ViewFamilyType>()
+                    .FirstOrDefault(x => x.ViewFamily == ViewFamily.ThreeDimensional);
+
+                var view3D = (viewFamilyType != null) ? View3D.CreateIsometric(doc, viewFamilyType.Id) : null;
+                if (view3D != null)
+                {
+                    Color white = new Color(255, 255, 255);
+                    view3D.SetBackground(ViewDisplayBackground.CreateGradient(white, white, white));
+                    views.Add(view3D.Id);
+
+                    var graphicDisplayOptions = view3D.get_Parameter(BuiltInParameter.MODEL_GRAPHICS_STYLE);
+
+                    graphicDisplayOptions.Set(6);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+
+            var ieo = new ImageExportOptions()
+            {
+                FilePath = tempImageFile, FitDirection = FitDirectionType.Horizontal,
+                HLRandWFViewsFileType = ImageFileType.PNG,
+                ImageResolution = ImageResolution.DPI_150,
+                ShouldCreateWebSite = false
+            };
+            if (views.Count > 0)
+            {
+                ieo.SetViewsAndSheets(views);
+                ieo.ExportRange = ExportRange.SetOfViews;
+            }
+            else
+            {
+                ieo.ExportRange = ExportRange.VisibleRegionOfCurrentView;
+            }
+
+            ieo.ZoomType = ZoomFitType.FitToPage;
+            ieo.ViewName = "tmp";
+
+            if (ImageExportOptions.IsValidFileName(tempImageFile))
+            {
+                try
+                {
+                    doc.ExportImage(ieo);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    throw;
+                }
+            }
+            else
+            {
+                return string.Empty;
+            }
+
+            var files = Directory.GetFiles(Path.GetTempPath(),
+                                           string.Format("{0}*.*", Path.GetFileNameWithoutExtension(tempFileName)));
+            return files.Length > 0 ? files[0] : string.Empty;
+        }
+
+        /// <summary>
+        /// Wrapper for old sample code.
+        /// </summary>
+        /// <param name="doc"></param>
+        /// <returns></returns>
+        static Result ExportToImage2(Document doc)
+        {
+            Result r = Result.Failed;
+            using (Transaction tx = new Transaction(doc))
+            {
+                tx.Start("Export Image");
+                string filepath = ExportToImage(doc);
+                tx.RollBack();
+                if (0 < filepath.Length)
+                {
+                    Process.Start(filepath);
+                    r = Result.Succeeded;
+                }
+            }
+            return r;
+        }
+
+        /// <summary>
+        /// New code as described in Revit API discussion
+        /// on how to export an image from a specific view using revit API C#
+        /// </summary>
+        /// <param name="doc"></param>
+        /// <returns></returns>
+        static Result ExportToImage3(Document doc)
+        {
+            Result r = Result.Failed;
+            using (Transaction tx = new Transaction(doc))
+            {
+                tx.Start("Export Image");
+                string destop_path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                View view = doc.ActiveView;
+                string filepath = Path.Combine(destop_path, view.Name);
+                ImageExportOptions img = new ImageExportOptions();
+
+                img.ZoomType = ZoomFitType.FitToPage;
+                img.PixelSize = 512;
+                img.ImageResolution = ImageResolution.DPI_600;
+                img.FitDirection = FitDirectionType.Horizontal;
+                img.ExportRange = ExportRange.CurrentView;
+                img.HLRandWFViewsFileType = ImageFileType.PNG;
+                img.FilePath = filepath;
+                img.ShadowViewsFileType = ImageFileType.PNG;
+
+                doc.ExportImage(img);
+                tx.RollBack();
+
+                filepath = Path.ChangeExtension(filepath, "png");
+                Process.Start(filepath);
+            }
+            return r;
+        }
+        #endregion
+
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            Document doc = uiapp.ActiveUIDocument.Document;
+            bool use_old_code = false;
+
+            Result r = use_old_code ? ExportToImage2(doc) : ExportToImage3(doc);
+            return r;
+        }
+    }
+}
